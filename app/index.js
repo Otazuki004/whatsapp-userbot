07dd07f09d2cc48e91c72dac857ba5cec19f5ba0
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2025-02-04 16:47:07 +0530

    Update index.js

--- app/index.js
+++ app/index.js
@@ -1,14 +1,8 @@
 import { Client } from 'whatsapp-web.js';
-import express from 'express';
-import qrcode from 'qrcode';
-
-const app = express();
 const client = new Client();
 
-let qrCodeUrl = '';
-
 client.on('qr', async qr => {
-    qrCodeUrl = await qrcode.toDataURL(qr);
+    console.log(qr);
 });
 
 client.on('ready', () => {
@@ -16,18 +10,3 @@ client.on('ready', () => {
 });
 
 client.initialize();
-
-app.get('/', (req, res) => {
-    res.send(`
-        <html>
-            <body style="display: flex; justify-content: center; align-items: center; height: 100vh; flex-direction: column;">
-                <h2>Scan the QR Code</h2>
-                <img src="${qrCodeUrl}" alt="QR Code"/>
-            </body>
-        </html>
-    `);
-});
-
-app.listen(8080, () => {
-    console.log('Server running on http://localhost:8080');
-});
