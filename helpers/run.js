05a783b4d955ffca7a29973c38708afe186f7c9c
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2025-02-05 15:08:45 +0530

    Update run.js

--- helpers/run.js
+++ helpers/run.js
@@ -1,24 +1,26 @@
-const { spawn, exec } = require('child_process');
+const { spawn } = require('child_process');
 
 function run(command) {
     return new Promise((resolve, reject) => {
-     try {   
-        const process = spawn(command);
-        let output = '';
+        try {
+            const process = spawn(command, { shell: true }); // Ensure it runs in a shell
+            let output = '';
 
-        process.stdout.on('data', (data) => {
-            output += data.toString();
-        });
+            process.stdout.on('data', (data) => {
+                output += data.toString();
+            });
 
-        process.stderr.on('data', (data) => {
-            output += data.toString();
-        });
+            process.stderr.on('data', (data) => {
+                output += data.toString();
+            });
 
-        process.on('close', (code) => {
-            resolve(output);
-        });
-    } catch (error) {
-        resolve(error)
+            process.on('close', (code) => {
+                resolve(output);
+            });
+        } catch (error) {
+            console.log(error);
+            reject(error);
+        }
     });
 }
 
